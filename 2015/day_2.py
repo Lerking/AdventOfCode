392d2453d2ffa1e4d85d75060b331eac495c9eb4
Lerking <33354709+Lerking@users.noreply.github.com>
2019-12-02 20:23:15 +0100

    Add files via upload

---
+++ 2015/day_2.py
@@ -0,0 +1,27 @@
+def calc(g):
+    return 2*g[0]*g[1]+2*g[1]*g[2]+2*g[0]*g[2]+g[0]*g[1]
+
+def rib(g):
+    return 2*g[0]+2*g[1]+g[0]*g[1]*g[2]
+    
+GIFTS = []
+wrapping = 0
+ribbon = 0
+
+with open("/storage/emulated/0/documents/adventorcode/2015/day_2_input.txt", 'r') as F:
+    temp = []
+    for line in F:
+        for v in line.split("x"):
+            temp.append(int(v))
+        temp.sort()
+        GIFTS.append(temp)
+        temp = []
+
+print(GIFTS)
+
+for v in GIFTS:
+    wrapping += calc(v)
+    ribbon += rib(v)
+
+print(wrapping)    #day 2, part 1
+print(ribbon)      #day 2, part 2
