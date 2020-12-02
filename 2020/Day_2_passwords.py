652c053586d92ee41f44cd6e574898cf9ee88bd1
Lerking <33354709+Lerking@users.noreply.github.com>
2020-12-02 12:25:42 +0100

    Update Day_2_passwords.py
    Updated Day 2 solution

--- 2020/Day_2_passwords.py
+++ 2020/Day_2_passwords.py
@@ -1,13 +1,21 @@
 '''
 Day 2. Help finding valid passwords.
-Part 1: Find number of valid passwords in input list.
-
+Part 1: Find number of valid passwords in input list, based on first rule set.
+Part 2: Find number of valid passwords, based on updated rule set.
 '''
 
 wd = 'C:\\Users\\dksojlg\\projects\\AdventOfCode\\2020'
 valid_passwords = []
 '''
 Part 1
+Test data:
+1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc
+Rule set:
+part 1, indicates lowest and highest number of instances (1-3).
+part 2, the instance (a:).
+part 3, the password (abcde).
 '''
 with open(wd+'\\passwords.txt', 'r') as ER:
     for item in ER:
@@ -22,22 +30,23 @@ print(f'Valid passwords using first rule set : {len(valid_passwords)}')
 
 '''
 Part 2
+Test data:
+1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc
+Rule set:
+part 1, indicates positions of instance (1-3). 1 and only 1 of the positions must contain
+part 2, the instance (a:).
+part 3, the password (abcde).
 '''
 valid_passwords = []
-valid = False
 with open(wd+'\\passwords.txt', 'r') as ER:
     for item in ER:
-        valid_1 = False
-        valid_2 = False
         nn, ins, psw = item.split(" ")
         psw = psw.strip("\n")
         index_1, index_2 = nn.split("-")
         instance = str(ins[0])
-        if psw[int(index_1)-1] == instance:
-            valid_1 = True
-        if psw[int(index_2)-1] == instance:
-            valid_2 = True
-        if valid_1 ^ valid_2:
+        if bool(psw[int(index_1)-1] == instance) ^ bool(psw[int(index_2)-1] == instance):
             valid_passwords.append(psw)
 
 print(f'Valid passwords using second rule set : {len(valid_passwords)}')
