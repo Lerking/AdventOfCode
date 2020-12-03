467b2938655b6b6b72705bcb101f609ff7a1141e
Lerking <33354709+Lerking@users.noreply.github.com>
2020-12-03 07:30:59 +0100

    Added documentation

--- 2020/Day_3_Trajectory.py
+++ 2020/Day_3_Trajectory.py
@@ -7,19 +7,19 @@ with open(wd+'\\tree_map.txt', 'r') as ER:
         tree_map.append(item.strip('\n'))
 
 line_len = len(tree_map[0])
-map_len = len(tree_map)-1
+map_len = len(tree_map)-1 #In order to not go beyond the map, we subtract 1 (remember indices starts at 0)
 
 def traverse(right, down):
-    line, col = 0, 0
+    line, col = 0, 0 # Start at top left corner of the map
     trees = 0
-    while line < map_len:
-        col += right
-        line += down
-        if col > line_len-1:
-            col = col % line_len
-        if tree_map[line][col] == '#':
-            trees += 1
-    return trees
+    while line < map_len: # As long as we're inside the map, we go on.
+        col += right # Accumulate column index
+        line += down # Accumulate line index
+        if col > line_len-1: # If col is larger than the lenght of the map line
+            col = col % line_len # We re-calculate the col index as modulo col/line length (this gives us the remainder, which is our new col index)
+        if tree_map[line][col] == '#': # If we encounter a tree, represented as '#' in the map
+            trees += 1 # We increase the number of trees we have encounted
+    return trees # When the end of the map has been reached, we return the number of trees encounted
 
 '''
 Part 1
@@ -37,7 +37,6 @@ right 7, down 1
 right 1, down 2
 multiply encountered trees in all trajections.
 '''
-
 trees = 1
 
 trees *= traverse(1,1)
