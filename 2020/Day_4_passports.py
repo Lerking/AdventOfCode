ae2bb6b2423347dc8836ab0185dde25c28f30709
Jan Lerking <jan.lerking@gmail.com>
2020-12-04 21:11:08 +0100

    Cleanup.

--- 2020/Day_4_passports.py
+++ 2020/Day_4_passports.py
@@ -1,19 +1,5 @@
 import re
 
-test_data = ['ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\n',
-            'byr:1937 iyr:2017 cid:147 hgt:183cm\n',
-            '\n',
-            'iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\n',
-            'hcl:#cfa07d byr:1929\n',
-            '\n',
-            'hcl:#ae17e1 iyr:2013\n',
-            'eyr:2024\n',
-            'ecl:brn pid:760753108 byr:1931\n',
-            'hgt:179cm\n',
-            '\n',
-            'hcl:#cfa07d eyr:2025 pid:166559648\n',
-            'iyr:2011 ecl:brn hgt:59in\n']
-
 wd = 'C:\\Users\\dksojlg\\projects\\AdventOfCode\\2020'
 
 with open('/home/jan/projects/AdventOfCode/2020/passports.txt', 'r') as ER:
@@ -30,10 +16,9 @@ with open('/home/jan/projects/AdventOfCode/2020/passports.txt', 'r') as ER:
 passports.append(passport)
 
 valids = [v for v in passports if len(v) == 8 or len(v) == 7 and 'cid' not in v]
-print(f'Number of valid passports : {len(valids)}')
+print(f'Part 1 : {len(valids)}')
 
 valid = 0
-new_valids = []
 for v in valids:
     if not 1920 <= int(v['byr']) <= 2002:
         continue
@@ -58,5 +43,4 @@ for v in valids:
         if not len(v['hgt']) == 5 or not 150 <= int(heads) <= 193:
             continue
     valid += 1
-    new_valids.append(v)
-print(len(new_valids))
+print(f'Part 2 : {valid}')
