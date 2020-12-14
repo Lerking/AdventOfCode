162533a8f74cc0a2a26e85db0c22d187429f0b76
Jan Lerking <jan.lerking@gmail.com>
2020-12-14 08:41:19 +0100

    Added part 2 solution.

--- 2020/Day_12_Navigation.py
+++ 2020/Day_12_Navigation.py
@@ -14,10 +14,10 @@ for n in test_data:
 with open('navigation.txt', 'r') as N:
     navi = [l.strip() for l in N.readlines()]
 
-def manhattan_distance(navi):
+def manhattan_distance(navigation):
     ship_direction = 'east'
     h, v = 0, 0
-    for n in navi:
+    for n in navigation:
         if n[0] == 'F':
             if ship_direction == 'east':
                 h += n[1]
@@ -108,11 +108,62 @@ def manhattan_distance(navi):
 
 print(f'Part 1 test: {manhattan_distance(nav)}')
 
-nav = []
+new_nav = []
 for n in navi:
     navl = []
     navl.append(n[0:1])
     navl.append(int(n[1:]))
-    nav.append(navl)
+    new_nav.append(navl)
 
-print(f'Part 1: {manhattan_distance(nav)}')
+print(f'Part 1: {manhattan_distance(new_nav)}')
+
+def new_manhattan_distance(navigation):
+    #ship_direction = 'east'
+    waypoint = [10, 1]
+    h, v = 0, 0
+    for n in navigation:
+        if n[0] == 'F':
+            h += n[1]*waypoint[0]
+            v += n[1]*waypoint[1]
+        elif n[0] == 'N':
+            waypoint[1] += n[1]
+            continue
+        elif n[0] == 'S':
+            waypoint[1] -= n[1]
+            continue
+        elif n[0] == 'E':
+            waypoint[0] += n[1]
+            continue
+        elif n[0] == 'W':
+            waypoint[0] -= n[1]
+            continue
+        elif n[0] == 'R':
+            if n[1] == 90:
+                tmp = []
+                tmp = [waypoint[1], -waypoint[0]]
+                waypoint[0], waypoint[1] = tmp[0], tmp[1]
+            elif n[1] == 180:
+                tmp = []
+                tmp = [-waypoint[0], -waypoint[1]]
+                waypoint[0], waypoint[1] = tmp[0], tmp[1]
+            elif n[1] == 270:
+                tmp = []
+                tmp = [-waypoint[1], waypoint[0]]
+                waypoint[0], waypoint[1] = tmp[0], tmp[1]
+        elif n[0] == 'L':
+            if n[1] == 90:
+                tmp = []
+                tmp = [-waypoint[1], waypoint[0]]
+                waypoint[0], waypoint[1] = tmp[0], tmp[1]
+            elif n[1] == 180:
+                tmp = []
+                tmp = [-waypoint[0], -waypoint[1]]
+                waypoint[0], waypoint[1] = tmp[0], tmp[1]
+            elif n[1] == 270:
+                tmp = []
+                tmp = [waypoint[1], -waypoint[0]]
+                waypoint[0], waypoint[1] = tmp[0], tmp[1]
+    return abs(h)+abs(v)
+
+print(f'Part 2 test: {new_manhattan_distance(nav)}')
+print(f'Part 2: {new_manhattan_distance(new_nav)}')
